af7409ed2942455df77cc90cd1b024ff43040850
cahersan <cahersan@gmail.com>
2014-03-13 20:39:21 +0100

    Included MANIFEST.in and updated setup.py

--- packr.py
+++ packr.py
@@ -1,29 +1,48 @@
-import os, sys
-import subprocess
+import os
+
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
+
 import setup_parser
 
-DEFAULT_SRCDIR = os.getcwd()
+INSTALL_DIR = 'DH_VIRTUALENV_INSTALL_ROOT'
+DEFAULT_INSTALL_DIR = '/usr/share/python/'
+TEMPLATES_DIR= os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')
 
 class Packr(object):
 
-    def __init__(self, srcdir, destdir=None, user=None,
-                    python=None):
-        if not srcdir:
-            self.srcdir = DEFAULT_SRCDIR
+    def __init__(self, srcdir, destdir, user, python):
+        self.srcdir = srcdir
         self.destdir = destdir
         self.user = user
         self.python = python
 
-    def fill_templates(self):
-        env = Environment(loader=FileSystemLoader('./templates'))
-
+    def setup(self):
+        # Read project setup.py
+        if not self.srcdir:
+            self.srcdir = os.getcwd()
         setup_file = os.path.join(self.srcdir, 'setup.py')
         package = setup_parser.parse_setup_file(setup_file)
 
-        control = env.get_template('control')
+        # Set relevant options
+        if self.destdir:
+            os.environ[INSTALL_DIR] = self.destdir
+        if not self.user:
+            self.user = package['name'] 
+        if self.python:
+            self.python = "--python " +  self.python
+        else:
+            self.python = ''
+            
+        # Get the templates
+        env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
+        control = env.get_template('control')
+        changelog = env.get_template('changelog')
+        preinst = env.get_template('preinst')
+        rules = env.get_template('rules')
+
+        # Fill in the templates
         self.control = control.render(
             name= package['name'],
             author=package['author'],
@@ -34,5 +53,39 @@ class Packr(object):
             arch = 'all',
         )
 
-    def print_control(self):
-        print(self.control)
+        self.changelog = changelog.render(
+            author=package['author'],
+            author_email=package['author_email'],
+            source=package['name'],
+        )
+
+        self.preinst = preinst.render(
+           home=os.environ.get(INSTALL_DIR,
+               os.path.join(DEFAULT_INSTALL_DIR, package['name'])),
+           user=self.user,
+        )
+        
+        self.rules = rules.render(
+           python=self.python,
+        )
+
+
+    def build(self):
+        pass
+
+#    @property 
+#    def control(self):
+#        return self.control
+#
+#    @property 
+#    def changelog(self):
+#        return self.changelog
+#
+#    @property 
+#    def postinst(self):
+#        return self.postinst
+#
+#    @property 
+#    def rules(self):
+#        return self.rules
+
