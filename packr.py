4481b6a4afa7e25cf55c3b42e58f9fe25079f6e2
Carlos de las Heras <cahersan@gmail.com>
2014-04-18 16:36:41 +0200

    More bug fixes, cleanup of tmp dir

--- packr.py
+++ packr.py
@@ -27,7 +27,7 @@ class Packr(object):
             self.srcdir = os.getcwd()
         setup_file = os.path.join(self.srcdir, 'setup.py')
         try:
-            package = setup_parser.parse_setup_file(setup_file)
+            self.package = setup_parser.parse_setup_file(setup_file)
         except FileNotFoundError:
             print(sys.exc_info()[1])
             exit()
@@ -37,7 +37,7 @@ class Packr(object):
             os.environ[INSTALL_DIR_ENV_VAR] = self.destdir
         
         if not self.user:
-            self.user = package['name'] 
+            self.user = self.package['name'] 
         
         if self.python:
             self.python = "--python " +  self.python
@@ -45,8 +45,10 @@ class Packr(object):
             self.python = ''
         
         # Create temporary build directory and copy project folder to it
-        self.tmp = tempfile.TemporaryDirectory()
-        shutil.copytree(self.srcdir, self.tmp)
+        self.tmpdir = tempfile.TemporaryDirectory()
+        self.tmp = self.tmpdir.name
+        self.tmp_project = os.path.join(self.tmp, self.package['name'])
+        shutil.copytree(self.srcdir, self.tmp_project)
 
         # Copy extra files to tmp and generate contents for install conffile
         if self.extra:
@@ -54,7 +56,7 @@ class Packr(object):
             inst = ''
             while self.extra:
                 f = self.extra.pop()
-                shutil.copyfile(f, self.tmp)
+                shutil.copy(f, self.tmp)
                 inst = inst + os.path.join(os.pardir, os.path.basename(f)) + \
                                                             ' ' + debdir + '\n'
             self.extra = inst
@@ -63,7 +65,7 @@ class Packr(object):
 
         # Project home, also home to the user created in preinst script 
         self.project_home=os.environ.get(INSTALL_DIR_ENV_VAR,
-                            os.path.join(DEFAULT_INSTALL_DIR, package['name']))
+                            os.path.join(DEFAULT_INSTALL_DIR, self.package['name']))
 
         # Get the templates
         env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
@@ -78,27 +80,27 @@ class Packr(object):
         dirs = env.get_template('dirs')
 
         # Make debian folder
-        debian_dir = os.path.join(self.srcdir, 'debian')
+        debian_dir = os.path.join(self.tmp_project, 'debian')
 
         if not os.path.exists(debian_dir):
             os.mkdir(debian_dir) 
         
         # Fill in the templates
         self.control = control.render(
-            name= package['name'],
-            author=package['author'],
-            author_email=package['author_email'],
-            url = package['url'],
-            description=package['description'],
-            source=package['name'],
+            name= self.package['name'],
+            author=self.package['author'],
+            author_email=self.package['author_email'],
+            url = self.package['url'],
+            description=self.package['description'],
+            source=self.package['name'],
             arch = 'all',
         )
 
         self.changelog = changelog.render(
-            author=package['author'],
-            author_email=package['author_email'],
-            source=package['name'],
-            version=package['version'],
+            author=self.package['author'],
+            author_email=self.package['author_email'],
+            source=self.package['name'],
+            version=self.package['version'],
         )
 
         self.preinst = preinst.render(
@@ -125,7 +127,7 @@ class Packr(object):
         )
 
         self.dirs = dirs.render(
-            name=package['name']
+            name=self.package['name']
         )
 
         # Other non-template file contents
@@ -148,16 +150,19 @@ class Packr(object):
     
     def build(self):
         build_dir = os.path.join(self.tmp, os.path.basename(self.srcdir))
-        pkgname = package['name'] + '_' + package['version'] + '_' + 'all.deb'
+        pkgname = self.package['name'] + '_' + self.package['version'] + '_' + 'all.deb'
         self.debpkg = os.path.join(self.tmp, pkgname) 
 
-        p = subprocess.Popen(['dpkg-buildpackage', '-us', '-uc'], cwd=build_dir)
+        p = subprocess.Popen(['dpkg-buildpackage', '-us', '-uc'], cwd=self.tmp_project)
         p.wait()
 
         if self.output:
             shutil.move(self.debpkg,  self.output)
         else:
             shutil.move(self.debpkg,  os.getcwd())
+        
+        self.tmpdir.cleanup()
+
 
 
 
